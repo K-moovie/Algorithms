77caf1f975e78e68d277906867a4afca61621863
K-moovie <kyha97@naver.com>
2020-07-14 11:19:56 +0900

    Solved 10989

--- B_10989.py
+++ B_10989.py
@@ -4,4 +4,16 @@ Date: 2020-07-14
 Title: 10989
 Language: Python 3
 """
+import sys
 
+array = [0] * 10001
+N = int(input())
+
+for i in range(0, N):
+    index = int(sys.stdin.readline())
+    array[index] += 1
+
+for i in range(0, 10001):
+    if array[i]:
+        for j in range(0, array[i]):
+            print(i)
