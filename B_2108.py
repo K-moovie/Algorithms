0709b526052af66e086ea1b8763cfc3a8a369c77
K-moovie <kyha97@naver.com>
2020-07-16 16:52:15 +0900

    Solving 2108

--- B_2108.py
+++ B_2108.py
@@ -6,41 +6,44 @@ Language: Python 3
 """
 
 import sys
-import math
 
 def get_middle(array, N):
-    count = -1
-    target_index = math.ceil(N / 2)
-    for i in range(0, len(array)):
-        for j in i:
-            if count == target_index:
-                return array[i][j]
-            count += 1
+    middle_index = N // 2 + 1
+    sum = 0
+    for i in range(-4000, 4001):
+        sum += array[i]
+        if middle_index <= sum:
+            return i
 
-def get_mode(array):
+def get_mode(array, N):
+    max_val = max(array)
+    if array.count(max_val) > 1:
+        new_array =[]
+        for i in range(-4000, 4001):
+            if array[i] == max_val:
+                new_array.append(i)
+        #print(new_array)
+        new_array.remove(min(new_array))
+        return min(new_array)
+    return array.index(max_val)
+
+def get_gap(array, N):
     new_array = []
-    for i in range(0, len(array)):
-        new_array.append(len(array[i]))
+    for i in range(-4000, 4001):
+        if array[i] != 0:
+            new_array.append(i)
+    return new_array[-1] - new_array[0]
 
-    min = min(new_array)
-    if new_array.count(min) > 1:
-        print(4)
-
-sum = 0
-array = [[] for i in range(8001)]
 N = int(sys.stdin.readline())
+array = [0 for i in range(0, 8001)]
+sum = 0
+
 for i in range(0, N):
-    input = int(sys.stdin.readline())
-    array[input].append(input)
-    sum += input
+    index = int(sys.stdin.readline())
+    sum += index
+    array[index] += 1
 
-new_array = []
-for i in range(0, len(array)):
-    if array[i]:
-        new_array.append(array[i])
-
-
-
-
-print(math.ceil(sum/N))
-
+print(round(sum / N))
+print(get_middle(array, N))
+print(get_mode(array, N))
+print(get_gap(array, N))
