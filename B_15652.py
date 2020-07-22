42c1d33b47b614e2f37fa479764d3bc49cfd7a73
K-moovie <kyha97@naver.com>
2020-07-22 20:07:23 +0900

    Solved 15652

--- B_15652.py
+++ B_15652.py
@@ -1,22 +1,23 @@
 """
 Author: Kim YeongHwa
 Date: 2020-07-22
-Title: 15651
+Title: 15652
 Language: Python 3
 """
 
-def dfs(depth):
+def dfs(depth, start):
     if depth == M:
         print(*arr)
         return
 
-    for i in range(N):
+    for i in range(start, N):
         arr[depth] = num_list[i]
-        dfs(depth + 1)
+        dfs(depth + 1, i)
+
 
 N, M = map(int, input().split())
 num_list = range(1, N + 1)
 visited = [False] * N
 arr = [0] * M
 
-dfs(0)
+dfs(0, 0)
