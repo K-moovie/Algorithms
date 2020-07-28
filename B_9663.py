4842aa945dc12d802fe07b8941fe57359f68873c
K-moovie <kyha97@naver.com>
2020-07-28 13:35:47 +0900

    Solved 9663

--- B_9663.py
+++ B_9663.py
@@ -6,47 +6,63 @@ Language: Python 3
 """
 import copy
 
-def is_possible(i, j):
-    visited_queue.append(copy.deepcopy(visited))
-    for a in range(N):
-        for b in range(N):
-            if (i == a) or (j == b):
-                visited[a][b] = True
-            if i + j == a + b:
-                visited[a][b] = True
-            if i - j + N - 1 == a - b + N - 1:
-                visited[a][b] = True
-
-
-def dfs(depth, x, y):
-    global count
-    global visited
-    if depth == N:
-        count += 1
-        print("count: " + str(count))
-        return
-
-    for i in range(x, N):
-        for j in range(y, N):
-            if visited[i][j]:
-                continue
-            is_possible(i, j)
-            print(i, j)
-            # if j == N - 1:
-            #     dfs(depth + 1, i + 1, 0)
-            #     visited = copy.deepcopy(visited_queue.pop())
-            #     print(visited)
-
-            dfs(depth, i, j + 1)
-            visited = copy.deepcopy(visited_queue.pop())
-            print(visited)
-    # check point: 탐색이 불완전함
-
-count = 0
+# def is_possible(i, j):
+#     visited_queue.append(copy.deepcopy(visited))
+#     # print()
+#     # print(visited_queue)
+#     for a in range(N):
+#         for b in range(N):
+#             if (i == a) or (j == b):
+#                 visited[a][b] = True
+#             if i + j == a + b:
+#                 visited[a][b] = True
+#             if i - j + N - 1 == a - b + N - 1:
+#                 visited[a][b] = True
+#
+# def dfs(depth, x):
+#     global count
+#     global visited
+#     global visited_queue
+#     if depth == N:
+#         count += 1
+#         # print("count: " + str(count))
+#         return
+#
+#     for i in range(x, N):
+#         for j in range(N):
+#             if visited[i][j]:
+#                 continue
+#             is_possible(i, j)
+#             # print("depth: " + str(depth))
+#             # print(i, j)
+#             # print(visited)
+#
+#             dfs(depth + 1, i + 1)
+#             visited = copy.deepcopy(visited_queue.pop())
+# count = 0
+#
+# N = int(input())
+# visited = [[False] * N for _ in range((int(N)))]
+# visited_queue = []
+#
+# dfs(0, 0)
+# print(count)
 
 N = int(input())
-visited = [[False] * N for _ in range((int(N)))]
-visited_queue = []
+count = 0
+row, diagonal_line_right, diagonal_line_left = [False] * N, [False] * (2 * N - 1), [False] * (2 * N - 1)
 
-dfs(0, 0, 0)
+
+def dfs(i):
+    global count
+    if i == N:
+        count += 1
+        return
+    for j in range(N):
+        if not (row[j] or diagonal_line_right[i + j] or diagonal_line_left[i - j + N - 1]):
+            row[j] = diagonal_line_right[i + j] = diagonal_line_left[i - j + N - 1] = True
+            dfs(i + 1)
+            row[j] = diagonal_line_right[i + j] = diagonal_line_left[i - j + N - 1] = False
+
+dfs(0)
 print(count)
--- test.py
+++ test.py
@@ -1,60 +1,77 @@
-import copy
+# import copy
+#
+#
+# count = 0
+#
+# N = int(input())
+# visited = [False for _ in range(N)]
+# visited_queue = []
+#
+#
+#
+# def is_possible(i):
+#     print("isPossible: ", end="")
+#     print(visited_queue)
+#     visited_queue.append(copy.deepcopy(visited))
+#     print("isPossible: " , end="")
+#     print(visited_queue)
+#     visited[i] = True
+#
+#
+# def dfs(depth):
+#     global count
+#     global visited
+#     if depth == N:
+#         count += 1
+#         print("count: " + str(count))
+#         return
+#
+#     for i in range(N):
+#         if visited[i]:
+#             continue
+#
+#         print(i)
+#         is_possible(i)
+#
+#
+#         dfs(depth + 1)
+#         visited = copy.deepcopy(visited_queue.pop())
+#         print(visited)
+#
+#
+# dfs(0)
+# print(count)
+#
+#
+# # a = [True]
+# # def b():
+# #     a[0] = False
+# #     print("B: ", end="")
+# #     print(a)
+# #
+# # def c():
+# #     b()
+# #     print(a)
+# #
+# # print(a)
+# # c()
+# #
+# # a.pop()
+# # a.pop()
 
+n, ans = int(input()), 0
+a, b, c = [False]*n, [False]*(2*n-1), [False]*(2*n-1)
 
-count = 0
-
-N = int(input())
-visited = [False for _ in range(N)]
-visited_queue = []
-
-
-
-def is_possible(i):
-    print("isPossible: ", end="")
-    print(visited_queue)
-    visited_queue.append(copy.deepcopy(visited))
-    print("isPossible: " , end="")
-    print(visited_queue)
-    visited[i] = True
-
-
-def dfs(depth):
-    global count
-    global visited
-    if depth == N:
-        count += 1
-        print("count: " + str(count))
+def solve(i):
+    global ans
+    if i == n:
+        ans += 1
         return
+    for j in range(n):
+        if not (a[j] or b[i+j] or c[i-j+n-1]):
+            a[j] = b[i+j] = c[i-j+n-1] = True
+            solve(i+1)
+            a[j] = b[i+j] = c[i-j+n-1] = False
 
-    for i in range(N):
-        if visited[i]:
-            continue
-
-        print(i)
-        is_possible(i)
-
-
-        dfs(depth + 1)
-        visited = copy.deepcopy(visited_queue.pop())
-        print(visited)
-
-
-dfs(0)
-print(count)
-
-
-# a = [True]
-# def b():
-#     a[0] = False
-#     print("B: ", end="")
-#     print(a)
-#
-# def c():
-#     b()
-#     print(a)
-#
-# print(a)
-# c()
-#
-# a.pop()
-# a.pop()
+solve(0)
+print(ans)
