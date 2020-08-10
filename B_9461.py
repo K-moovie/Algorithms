92ec730ddb184b24121845a2943512c8bcb8482d
K-moovie <kyha97@naver.com>
2020-08-10 20:18:27 +0900

    Solved 9461

--- B_9461.py
+++ B_9461.py
@@ -4,3 +4,21 @@ Date: 2020-08-10
 Title: 9461
 Language: Python 3
 """
+
+def make_padovan():
+    padovan[0] = 1
+    padovan[1] = 1
+    padovan[2] = 1
+
+    for i in range(3, 100):
+        padovan[i] = padovan[i - 2] + padovan[i - 3]
+
+padovan = [0] * 100
+make_padovan()
+test_case = int(input())
+for _ in range(test_case):
+    N = int(input())
+    print(padovan[N - 1])
+
+
+
