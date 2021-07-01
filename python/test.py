58aa54034a6be909861af59f6988919c3e6cf974
K-moovie <kyha97@naver.com>
2021-07-02 00:55:01 +0900

    Add Programmers/2017 카카오코드본선/단체사진 찍기

--- python/test.py
+++ python/test.py
@@ -1,5 +1,20 @@
-from collections import deque
-from bisect import bisect
+def sol():
+    pass
 
-bi = bisect([1, 2, 3, 9, 12, 10], 13)
-print(bi)
+def dfs(depth):
+    if depth == 8:
+        # print(case)
+        return
+
+    for i, v in enumerate(visited):
+        if not v:
+            visited[i] = True
+            case[depth] = i
+            if depth >= 7:
+                print(depth)
+            dfs(depth+1)
+            visited[i] = False
+
+visited = list(False for _ in range(8))
+case = list(range(8))
+dfs(0)
