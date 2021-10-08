034e754862db753e494b8dbeba6021d2416d7a01
K-moovie <kyha97@naver.com>
2021-10-08 22:09:03 +0900

    Add: Programmers 그래프 가장 먼 노드

---
+++ python/P_그래프_가장먼노드.py
@@ -0,0 +1,69 @@
+"""
+Author: Kim YeongHwa
+Date: 2021-10-08
+Title: Programmers/그래프/가장 먼 노드
+Language: Python 3
+Contents:
+    문제 설명
+    n개의 노드가 있는 그래프가 있습니다. 각 노드는 1부터 n까지 번호가 적혀있습니다. 1번 노드에서 가장 멀리 떨어진 노드의 갯수를 구하려고 합니다. 가장 멀리 떨어진 노드란 최단경로로 이동했을 때 간선의 개수가 가장 많은 노드들을 의미합니다.
+    노드의 개수 n, 간선에 대한 정보가 담긴 2차원 배열 vertex가 매개변수로 주어질 때, 1번 노드로부터 가장 멀리 떨어진 노드가 몇 개인지를 return 하도록 solution 함수를 작성해주세요.
+
+    제한사항
+    노드의 개수 n은 2 이상 20,000 이하입니다.
+    간선은 양방향이며 총 1개 이상 50,000개 이하의 간선이 있습니다.
+    vertex 배열 각 행 [a, b]는 a번 노드와 b번 노드 사이에 간선이 있다는 의미입니다.
+
+    입출력 예
+    n	vertex	return
+    6	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]	3
+
+    입출력 예 설명
+    예제의 그래프를 표현하면 아래 그림과 같고, 1번 노드에서 가장 멀리 떨어진 노드는 4,5,6번 노드입니다.
+"""
+
+from collections import defaultdict
+from collections import deque
+from collections import Counter
+
+
+def solution(n, edges):
+    answer = 0
+    graph = defaultdict(list)
+    for edge in edges:
+        left, right = edge
+        graph[left].append(right)
+        graph[right].append(left)
+
+    def bfs(start):
+        visited = [0 for _ in range(n + 1)]
+        queue = deque()
+        queue.append(start)
+
+        while queue:
+            cur_node = queue.popleft()
+            distance = visited[cur_node] + 1
+            for node in graph[cur_node]:
+                if visited[node] > 0:
+                    continue
+                queue.append(node)
+                visited[node] += distance
+
+        visited[start] = 0
+        max_distance = max(visited)
+        return Counter(visited)[max_distance]
+
+    answer = bfs(1)
+    return answer
+
+
+if __name__ == '__main__':
+    n = 6
+    vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+    expected = 3
+    result = solution(n, vertex)
+
+    if expected == result:
+        print("정답입니다.")
+    else:
+        print("오답입니다.")
+    print('Expected: {0}\nResult: {1} '.format(expected, result))
