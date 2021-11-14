04744e90f69c6a294b592ab99923aa9455f7d3ab
K-moovie <kyha97@naver.com>
2021-11-14 15:19:48 +0900

    [Add] Programmers 해시 베스트앨범

---
+++ python/P_해시_베스트앨범.py
@@ -0,0 +1,73 @@
+"""
+Author: Kim YeongHwa
+Date: 2021-11-14
+Title: Programmers/해시/베스트앨범
+Language: Python 3
+Contents:
+    문제 설명
+    스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
+
+    속한 노래가 많이 재생된 장르를 먼저 수록합니다.
+    장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+    장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+    노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
+
+    제한사항
+    genres[i]는 고유번호가 i인 노래의 장르입니다.
+    plays[i]는 고유번호가 i인 노래가 재생된 횟수입니다.
+    genres와 plays의 길이는 같으며, 이는 1 이상 10,000 이하입니다.
+    장르 종류는 100개 미만입니다.
+    장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
+    모든 장르는 재생된 횟수가 다릅니다.
+    입출력 예
+    genres	plays	return
+    ["classic", "pop", "classic", "classic", "pop"]	[500, 600, 150, 800, 2500]	[4, 1, 3, 0]
+    입출력 예 설명
+    classic 장르는 1,450회 재생되었으며, classic 노래는 다음과 같습니다.
+
+    고유 번호 3: 800회 재생
+    고유 번호 0: 500회 재생
+    고유 번호 2: 150회 재생
+    pop 장르는 3,100회 재생되었으며, pop 노래는 다음과 같습니다.
+
+    고유 번호 4: 2,500회 재생
+    고유 번호 1: 600회 재생
+    따라서 pop 장르의 [4, 1]번 노래를 먼저, classic 장르의 [3, 0]번 노래를 그다음에 수록합니다.
+
+    ※ 공지 - 2019년 2월 28일 테스트케이스가 추가되었습니다.
+"""
+from collections import defaultdict
+
+
+def solution(genres, plays):
+    answer = []
+    top_genre = defaultdict(int)
+    top_play = defaultdict(list)
+
+    for index, genre in enumerate(genres):
+        top_genre[genre] += plays[index]
+        top_play[genre].append((index, plays[index]))
+    top_genre = sorted(top_genre.items(), key=lambda x: x[1], reverse=True)
+
+    for genre, plays in top_play.items():
+        temp = sorted(plays, key=lambda x: x[1], reverse=True)
+        top_play[genre] = temp
+
+    for genre, play in top_genre:
+        length = 2 if len(top_play[genre]) >= 2 else 1
+        list(map(lambda x: answer.append(x[0]), top_play[genre][0:length]))
+
+    return answer
+
+
+if __name__ == '__main__':
+    genres = ["classic", "pop", "classic", "classic", "pop"]
+    plays = [500, 600, 150, 800, 2500]
+    expected = [4, 1, 3, 0]
+    result = solution(genres, plays)
+
+    if expected == result:
+        print("정답입니다.")
+    else:
+        print("오답입니다.")
+    print('Expected: {0}\nResult: {1} '.format(expected, result))
