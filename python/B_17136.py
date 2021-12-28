e4be644f367c12b17dd26e85014efd04dda47355
K-moovie <kyha97@naver.com>
2021-12-29 00:17:55 +0900

    [Revise] Baekjoon 17136 색종이 붙이기

--- python/B_17136.py
+++ python/B_17136.py
@@ -25,60 +25,58 @@ Output:
 """
 import sys
 
-def check(i,j,size,table):
-    target = []
-    for y in range(size) :
-        for x in range(size) :
-            yy, xx = i+y, j+x
-            if yy < 0 or yy >= 10 or xx < 0 or xx >= 10 :
-                return False, []
+sys.setrecursionlimit(10000000)
+input = sys.stdin.readline
 
-            if table[yy][xx] == 0 :
-                return False, []
-            else :
-               target.append((yy, xx))
 
-    temp = [[table[y][x] for x in range(10)] for y in range(10)]
-    for y,x in target:
-        temp[y][x] = 0
+def dfs(y, x, count):
+    global answer, used
 
-    return True, temp
-
-def DFS(table, rest_cnt, papers):
-    global result
-    if rest_cnt == 0 :
-        result = min(result,25 - sum(papers))
+    if y >= 10:
+        answer = min(answer, count)
         return
-    #print(table,rest_cnt,papers)
-    for i in range(10):
-        for j in range(10):
-            if table[i][j] == 1:
-                flag = False
-                for size in range(1,6):
-                    if papers[size] > 0:
-                        check_result, temp = check(i,j,size,table)
-                        if check_result:
-                            papers[size] -= 1
-                            DFS(temp,rest_cnt-(size**2),papers)
-                            papers[size] += 1
-                        flag = True
-                if flag:
-                    return
 
-table = [list(map(int,sys.stdin.readline().split())) for _ in range(10)]
-rest_cnt = 0
+    if x >= 10:
+        dfs(y + 1, 0, count)
+        return
 
-for i in range(10):
-    for j in range(10):
-        if table[i][j] == 1 :
-            rest_cnt += 1
+    # 현재 좌표가 1일 때
+    # 색종이 붙이기
+    if paper[y][x] == 1:
+        for k in range(5):
+            if used[k] == 5:
+                continue
+            if y + k >= 10 or x + k >= 10:
+                continue
 
-papers = [0,5,5,5,5,5]
-result = 26
+            flag = True
+            for i in range(y, y + k + 1):
+                for j in range(x, x + k + 1):
+                    if paper[i][j] == 0:
+                        flag = False
+                        break
+                if not flag:
+                    break
+            if flag:
+                for i in range(y, y + k + 1):
+                    for j in range(x, x + k + 1):
+                        paper[i][j] = 0
+                used[k] += 1
+                dfs(y, x + k + 1, count + 1)
+
+                used[k] -= 1
+                for i in range(y, y + k + 1):
+                    for j in range(x, x + k + 1):
+                        paper[i][j] = 1
+    # 현재 좌표가 0이라면
+    # 옆으로 넘어간다.
+    else:
+        dfs(y, x + 1, count)
 
 
-DFS(table, rest_cnt, papers)
-if result == 26:
-    print(-1)
-else:
-    print(result)
+paper = [list(map(int, input().split())) for _ in range(10)]
+used = [0] * 5
+answer = 26
+
+dfs(0, 0, 0)
+print(answer if answer != 26 else -1)
