4d197c45afbbfb46284dab282eaf276280512f07
K-moovie <kyha97@naver.com>
2022-02-05 00:02:42 +0900

    [Add] LeetCode Reverse Integer

---
+++ java/L_RevserseInteger.java
@@ -0,0 +1,44 @@
+/*
+Author: Kim YeongHwa
+Date: 2022-02-05
+Title: LeetCode/7. Reverse Integer
+Language: Java 11
+Contents:
+    7. Reverse Integer
+    Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+    Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+    Example 1:
+    Input: x = 123
+    Output: 321
+
+    Example 2:
+    Input: x = -123
+    Output: -321
+
+    Example 3:
+    Input: x = 120
+    Output: 21
+
+
+    Constraints:
+    -231 <= x <= 231 - 1
+*/
+
+class Solution {
+    public int reverse(int x) {
+        long answer = 0;
+
+        while (x != 0) {
+            answer *= 10;
+            answer += (x % 10);
+            x /= 10;
+        }
+
+        if (answer > Integer.MAX_VALUE || answer < Integer.MIN_VALUE) {
+            answer = 0;
+        }
+
+        return (int)answer;
+    }
+}
